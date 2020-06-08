f8cf6f051eb78993a181fc84944286bed0d7e4d9
lokesh inumpudi <lokesh.inumpudi@games24x7.com>
2020-06-08 23:59:52 +0530

    feat(initialcommit): init commit

---
+++ function/__init__.py
@@ -0,0 +1,30 @@
+import logging
+import azure.functions as func
+import psycopg2
+import os
+from datetime import datetime
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+
+def main(msg: func.ServiceBusMessage):
+
+    notification_id = int(msg.get_body().decode('utf-8'))
+    logging.info(
+        'Python ServiceBus queue trigger processed message: %s', notification_id)
+
+    # TODO: Get connection to database
+
+    try:
+        # TODO: Get notification message and subject fron database using the notification_id
+
+        # TODO: Get attendees email and name
+
+        # TODO: Loop thru each attendee and send an email with a personalized subject
+
+        # TODO: Update the notification table by setting the completed date and updating the status with the total number of attendees notified
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        logging.error(error)
+    finally:
+        # TODO: Close connection
